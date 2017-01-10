8e3c6e38f5338db74b2fa0265c51e5156f832e21
Pranav Mayuram <pranav.mayuram@gmail.com>
2017-01-10 13:40:06 -0500

    Add in new bootstrap theming and begin the login page

--- ui/js/crowdplay.js
+++ ui/js/crowdplay.js
@@ -3,10 +3,12 @@ var crowdPlay = angular.module('crowdPlay', []);
 crowdPlay.run(function($rootScope) {
     $rootScope.baseName= "CrowdPlay";
     $rootScope.channelSocket = io();
+    console.log("makin waves");
 });
 
 crowdPlay.controller('loginController', function($scope, $rootScope, $http, $window) {
-    $scope.connectSocket = function(channelToJoin) {
+    $scope.joinChannel = function(channelToJoin) {
+        console.log("leggo Join");
         $rootScope.channelSocket.on('connect', function(response) {
            // Connected, let's sign-up for to receive messages for this room
            if (response) {
@@ -16,6 +18,20 @@ crowdPlay.controller('loginController', function($scope, $rootScope, $http, $win
                console.log("lel");
            }
            $rootScope.channelSocket.emit('joinChannel', channelToJoin);
+        });
+    };
+    
+    $scope.createChannel = function(channelToCreate) {
+        console.log("leggo Create");
+        $rootScope.channelSocket.on('connect', function(response) {
+           // Connected, let's sign-up for to receive messages for this room
+           if (response) {
+               console.log(response);
+           }
+           else {
+               console.log("lel");
+           }
+           $rootScope.channelSocket.emit('createChannel', channelToCreate);
         });
     };
 });
